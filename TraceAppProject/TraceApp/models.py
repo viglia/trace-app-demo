7b38e63f7bb65d1e8ba1831d46a91cc9113e3c34
Francesco Vigliaturo <francescovigliaturo@fmpb.local>
2020-08-19 14:56:41 +0200

    adds Trace model to models.py

--- TraceAppProject/TraceApp/models.py
+++ TraceAppProject/TraceApp/models.py
@@ -1,1 +1,7 @@
 from django.db import models
+
+
+class Trace(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.PositiveIntegerField(primary_key=True)
+    trace = models.TextField()
